f32f68385bda77c0bc2065986fbb34153b2300d9
bukidai <k022c0054@g.neec.ac.jp>
2023-11-24 14:58:12 +0900

    gui対応

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from BMICalculator import BMICalculator
+import tkinter as tk
 
 
-def main():
-    height = float(input("Enter height in meters: "))
-    weight = float(input("Enter weight in kilograms: "))
-    bmiCalc = BMICalculator(height=height, weight=weight)
-    print("BMI is", bmiCalc.calculateBMI(), bmiCalc.getBMI())
+class BMICalculator:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("BMI Calculator")
+
+        self.label_height = tk.Label(root, text="身長（cm）：")
+        self.label_height.pack()
+
+        self.entry_height = tk.Entry(root)
+        self.entry_height.pack()
+
+        self.label_weight = tk.Label(root, text="体重（kg）：")
+        self.label_weight.pack()
+
+        self.entry_weight = tk.Entry(root)
+        self.entry_weight.pack()
+
+        self.button_calculate = tk.Button(root, text="計算", command=self.calculate_bmi)
+        self.button_calculate.pack()
+
+        self.label_result = tk.Label(root, text="")
+        self.label_result.pack()
+
+    def calculate_bmi(self):
+        height = float(self.entry_height.get())
+        weight = float(self.entry_weight.get())
+
+        bmi = weight / ((height / 100) ** 2)
+
+        result = f"あなたのBMIは {bmi:.2f} です。"
+
+        self.label_result.config(text=result)
 
 
-if __name__ == "__main__":
-    main()
+root = tk.Tk()
+bmi_calculator = BMICalculator(root)
+root.mainloop()
